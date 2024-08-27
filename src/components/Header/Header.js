c6f28fb414b8a048d32e528b10971ca2a027f8af
HiiTuongSing <117565834+HiiTuongSing@users.noreply.github.com>
2024-08-27 18:04:32 +0800

    Update: Added states to header nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,22 @@ import { useMediaQuery } from "react-responsive";
 function Header() {
   const isMobile = useMediaQuery({ query: "(max-width: 900px)" });
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [category, setCategory] = useState(null);
+  const [keywords, setKeywords] = useState("");
   const menuRef = useRef(null);
   const toggleRef = useRef(null);
 
+  console.log(keywords);
+  console.log(category);
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleReset = () => {
+    setIsMenuOpen(false);
+    setCategory(null);
+    setKeywords("");
   };
 
   const handleClickOutside = (e) => {
@@ -32,20 +43,28 @@ function Header() {
   return (
     <div className="w-[100vw] h-[10vh]">
       <div className="flex w-[98%] lg:w-[80%] m-auto justify-between h-full items-center relative">
-        <h1>My News</h1>
+        <button type="button" onClick={handleReset}>
+          My News
+        </button>
+
         {!isMobile ? (
           <>
             <ul className="flex w-[60%] justify-between">
-              <li>Malaysia</li>
-              <li>General</li>
-              <li>Health</li>
-              <li>Sports</li>
-              <li>Business</li>
-              <li>Technology</li>
-              <li>Science</li>
+              <li onClick={() => setCategory("general")}>General</li>
+              <li onClick={() => setCategory("health")}>Health</li>
+              <li onClick={() => setCategory("sports")}>Sports</li>
+              <li onClick={() => setCategory("business")}>Business</li>
+              <li onClick={() => setCategory("technology")}>Technology</li>
+              <li onClick={() => setCategory("science")}>Science</li>
             </ul>
             <form>
-              <input type="text" placeholder="Search news..." />
+              <input
+                name="search"
+                type="text"
+                value={keywords}
+                onChange={(e) => setKeywords(e.target.value)}
+                placeholder="Search news..."
+              />
               <button type="submit">Search</button>
             </form>
           </>
@@ -60,18 +79,23 @@ function Header() {
             role="menu"
             className="absolute left-[0px] top-[100%] w-full p-2 bg-red-200"
           >
-            <form className="w-full">
-              <input type="text" placeholder="Search news..." />
+            <form>
+              <input
+                name="search"
+                type="text"
+                value={keywords}
+                onChange={(e) => setKeywords(e.target.value)}
+                placeholder="Search news..."
+              />
               <button type="submit">Search</button>
             </form>
             <ul className=" w-[60%] ">
-              <li>Malaysia</li>
-              <li>General</li>
-              <li>Health</li>
-              <li>Sports</li>
-              <li>Business</li>
-              <li>Technology</li>
-              <li>Science</li>
+              <li onClick={() => setCategory("general")}>General</li>
+              <li onClick={() => setCategory("health")}>Health</li>
+              <li onClick={() => setCategory("sports")}>Sports</li>
+              <li onClick={() => setCategory("business")}>Business</li>
+              <li onClick={() => setCategory("technology")}>Technology</li>
+              <li onClick={() => setCategory("science")}>Science</li>
             </ul>
           </div>
         ) : null}
