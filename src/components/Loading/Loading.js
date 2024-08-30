8418d415cafe85c2545f65e5dc7352815ada4b75
HiiTuongSing <117565834+HiiTuongSing@users.noreply.github.com>
2024-08-30 18:20:53 +0800

    Update: Refactored MoreNews component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,5 +1,6 @@
 const Loading = () => {
   return (
+    // Set parent component to relative to avoid error
     <div className="absolute top-0 left-0 flex justify-center items-center w-full h-full bg-white/50 z-10">
       <h1 className="animate-pulse bg-blue-500 text-white px-4 py-2 rounded">
         Loading ...
--- src/components/MoreNews/MoreNews.js
+++ src/components/MoreNews/MoreNews.js
@@ -1,78 +1,41 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-import NewsCard from "../NewsCard/NewsCard";
 import { useParams } from "react-router-dom";
+import { capitalizeFirstLetter } from "../../utils/utils";
+import { useFetchNews } from "../../hooks/hooks";
+import NewsCard from "../NewsCard/NewsCard";
+import Loading from "../Loading/Loading";
 
 function MoreNews() {
-  const [articles, setArticles] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [page, setPage] = useState(1);
-
   const category = useParams();
-
-  const test = () => {
-    setPage(1);
-    fetchByCategory(category, page);
-  };
-
-  const fetchByCategory = async (category, page) => {
-    setIsLoading(true);
-    try {
-      const response = await axios.get(
-        `https://newsapi.org/v2/everything?q=${category.id}&pageSize=16&page=${page}&sortBy=relativity&apiKey=0b3d3b7a151b4952b93507d2b9a71bde`
-      );
-      setIsLoading(false);
-      setArticles(response.data.articles);
-    } catch (err) {
-      setIsLoading(false);
-      console.error(err);
-    }
-  };
-
-  const loadMore = async (category, page) => {
-    setIsLoading(true);
-    try {
-      const response = await axios.get(
-        `https://newsapi.org/v2/everything?q=${category.id}&pageSize=16&page=${page}&sortBy=relativity&apiKey=0b3d3b7a151b4952b93507d2b9a71bde`
-      );
-      setIsLoading(false);
-      setArticles((prev) => [...prev, ...response.data.articles]);
-    } catch (err) {
-      setIsLoading(false);
-      console.error(err);
-    }
-  };
+  const [page, setPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState([]);
+  // let url = `https://newsapi.org/v2/everything?q=${category.id}&pageSize=16&page=${page}&sortBy=relativity&apiKey=0b3d3b7a151b4952b93507d2b9a71bde`;
+  let url = "/testapi16.json";
+  const { articles, isLoading, error } = useFetchNews(url);
 
   const handleLoadMore = () => {
     setPage(page + 1);
   };
 
   useEffect(() => {
-    if (page === 1) return;
-    loadMore(category, page);
-  }, [page]);
-
-  function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-  // useEffect(() => {
-  //   setPage(1);
-  //   setArticles([]);
-  //   fetchByCategory(category);
-  // }, [category]);
+    if (page === 1) {
+      setCurrentPage(articles);
+    } else {
+      setCurrentPage((previousPage) => [...previousPage, ...articles]);
+    }
+  }, [articles]);
 
   return (
-    <fieldset className="grid grid-cols-2 sm:grid-cols-4 gap-2 border-2 p-2 border-2 p-2 border-blue-500 w-full">
+    <fieldset className="grid grid-cols-2 sm:grid-cols-4 gap-2 border-2 p-2 border-2 p-2 border-blue-500 w-full relative">
       <legend className="flex w-full justify-between text-xl font-bold px-4 text-blue-500">
         Category: {capitalizeFirstLetter(category.id)}
-        {/* <button onClick={() => test()}>Load</button> */}
       </legend>
-      {isLoading && <h1 className="animate-pulse">Loading ...</h1>}
-      {articles.length > 0 &&
-        articles.map((article, index) => {
+      {isLoading && <Loading />}
+      {currentPage.length > 0 &&
+        currentPage.map((article, index) => {
           return <NewsCard key={index} article={article} />;
         })}
-      {articles.length > 0 && (
+      {currentPage.length > 0 && (
         <div className="flex w-full justify-center sm:col-span-4 col-span-2">
           <button
             type="button"
