7dbe2efaf44658700873a8886dace5af9f8f1c41
HiiTuongSing <117565834+HiiTuongSing@users.noreply.github.com>
2024-08-30 11:50:11 +0800

    New: Added filter function

--- src/components/DisplaySearchResult/DisplaySearchResult.js
+++ src/components/DisplaySearchResult/DisplaySearchResult.js
@@ -4,10 +4,17 @@ import axios from "axios";
 import NewsCard from "../NewsCard/NewsCard";
 
 export default function DisplaySearchResult() {
+  const today = new Date().toISOString().split("T")[0];
   const keywords = useParams();
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [articles, setArticles] = useState([]);
+  const [filter, setFilter] = useState({
+    category: keywords.id,
+    dateStart: today,
+    dateEnd: today,
+    source: "",
+  });
 
   const fetchByCategory = async (keywords, page) => {
     setIsLoading(true);
@@ -41,6 +48,26 @@ export default function DisplaySearchResult() {
     }
   };
 
+  const handleFilter = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await axios.get(
+        `https://newsapi.org/v2/everything?q=${
+          filter.category === "" ? keywords.id : filter.category
+        }&from=${filter.dateStart}&to=${filter.dateEnd}${
+          filter.source
+        }&pageSize=16&page=${page}&sortBy=relativity&apiKey=0b3d3b7a151b4952b93507d2b9a71bde`
+      );
+      setIsLoading(false);
+      setArticles(response.data.articles);
+      if (!response.data.length > 0)
+        alert("No results found, try changing the filter.");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const handleLoadMore = () => {
     setPage(page + 1);
   };
@@ -58,6 +85,49 @@ export default function DisplaySearchResult() {
 
   return (
     <div className="w-[90%] m-auto">
+      <div>
+        <form onSubmit={(e) => handleFilter(e)}>
+          <label>Category</label>
+          <select
+            value={filter.category}
+            onChange={(e) => setFilter({ ...filter, category: e.target.value })}
+          >
+            <option value="">Select Category</option>
+            <option value="technology">Technology</option>
+            <option value="science">Science</option>
+            <option value="business">Business</option>
+            <option value="health">Health</option>
+          </select>
+          <label>Date Range</label>
+          <input
+            type="date"
+            value={filter.dateStart}
+            onChange={(e) =>
+              setFilter({ ...filter, dateStart: e.target.value })
+            }
+          />
+          <input
+            type="date"
+            value={filter.dateEnd}
+            onChange={(e) => setFilter({ ...filter, dateEnd: e.target.value })}
+          />
+          <label>Source</label>
+          <select
+            value={filter.source}
+            onChange={(e) => setFilter({ ...filter, source: e.target.value })}
+          >
+            <option value="">Select Source</option>
+            <option value="&domains=thestar.com.my">The Star</option>
+            <option value="&domains=techcrunch.com">Tech Crunch</option>
+            <option value="&domains=thenextweb.com">The Next Web</option>
+            <option value="&domains=nst.com.my">New Straits Times</option>
+            <option value="&domains=theedgemarkets.com">
+              The Edge Markets
+            </option>
+          </select>
+          <button type="submit">Apply Filters</button>
+        </form>
+      </div>
       <h1>Showing Results for {keywords.id}</h1>
       <button onClick={() => test()}>Load</button>
       {isLoading && <h1 className="animate-pulse">Loading ...</h1>}
@@ -68,24 +138,6 @@ export default function DisplaySearchResult() {
           })}
       </div>
       <button onClick={() => handleLoadMore()}>Load more...</button>
-      <div>
-        <form>
-          <label>Category</label>
-          <select>
-            <option>Technology</option>
-            <option>Science</option>
-            <option>Business</option>
-            <option>Health</option>
-          </select>
-          <label>Date Range</label>
-          <input type="date" />
-          <input type="date" />
-          <label>Source</label>
-          <select>
-            <option>The Star</option>
-          </select>
-        </form>
-      </div>
     </div>
   );
 }
